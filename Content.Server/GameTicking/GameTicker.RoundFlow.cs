a2ab026da80fc9ede4b452d6d46e0c5686fdc4ee
Голубь <gerimil109@gmail.com>
2026-02-27 22:01:44 +0700

    Better

--- Content.Server/GameTicking/GameTicker.RoundFlow.cs
+++ Content.Server/GameTicking/GameTicker.RoundFlow.cs
@@ -377,6 +377,7 @@ namespace Content.Server.GameTicking
                 return;
 
             _startingRound = true;
+            RoundStartTimeSpan = _gameTiming.CurTime;
 
             if (RoundId == 0)
                 IncrementRoundNumber();
@@ -456,7 +457,6 @@ namespace Content.Server.GameTicking
             _roundStartDateTime = DateTime.UtcNow;
             RunLevel = GameRunLevel.InRound;
 
-            RoundStartTimeSpan = _gameTiming.CurTime;
             SendStatusToAll();
             ReqWindowAttentionAll();
             UpdateLateJoinStatus();
